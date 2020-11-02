0da34f999e98b82a466f2b3b6f898fc0ce58dc24
mathlete01 <msallin@gmail.com>
2020-11-02 15:39:10 -0800

    Done.

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,38 @@
 class EmployeesController < ApplicationController
+
+    def index
+        @employees = Employee.all
+    end
+
+    def show
+        @employee = Employee.find(params[:id])
+    end
+
+    def edit
+        @employee = Employee.find(params[:id])
+        @dog = @employee.dog
+    end
+
+    def new
+        @employee = Employee.new
+    end
+
+    def create
+        @employee = Employee.new(params.require(:employee).permit(:first_name, :last_name, :alias, :title, :office, :dog_id))
+        if @employee.save
+            redirect_to employee_path(@employee)
+        else
+            render :new
+        end
+    end
+
+    def update
+        @employee = Employee.find(params[:id])
+        if @employee.update(params.require(:employee).permit(:first_name, :last_name, :alias, :title, :office, :dog_id))
+            redirect_to employee_path(@employee)
+        else
+            render :new
+        end
+    end
+
 end
--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,2 +1,8 @@
 class Employee < ApplicationRecord
+    belongs_to :dog
+
+    validates :alias, uniqueness: true
+    validates :title, uniqueness: true
+
+    #full_name = "#{self.first_name} #{self.last_name}"
 end
