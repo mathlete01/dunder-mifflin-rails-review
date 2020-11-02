7f25b7576f3952cea226833c0da9f36bbf82cc1e
mathlete01 <msallin@gmail.com>
2020-11-02 15:13:07 -0800

    commit tree 7c41fa1b815f18d4bb666ab9b615ccfd137130ae

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,2 +1,9 @@
 class Employee < ApplicationRecord
+    belongs_to :dog
+
+    validates :alias, uniqueness: true
+    validates :job_title, uniqueness: true
+
+    #full_name = "#{self.first_name} #{self.last_name}"
 end
+    
